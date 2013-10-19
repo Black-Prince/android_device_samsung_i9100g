4839eba78a49f65255061ec1f92f5bdbd341e7ef
Black-Prince <shreekant.nagre@gmail.com>
2013-10-19 21:48:49 +0530

    We dont want copy this file now , omni vendor had inclusion

--- omni_i9100g.mk
+++ omni_i9100g.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
-
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9100G
 
